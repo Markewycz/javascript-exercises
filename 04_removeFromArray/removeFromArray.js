f214291f471ef63aa10e4116336a3f831685e2ff
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-04-30 14:10:08 +0200

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...numbers) {
+  // for (num of numbers) {
+  //   for (arrNum of arr) {
+  //     if (arrNum === num) {
+  //       arrNum -= 1;
+  //       arr.splice(arrNum, 1);
+  //     }
+  //   }
+  // }
+  const newArray = [];
 
+  arr.forEach((item) => {
+    if (!numbers.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
-
+// Almost got it, forgot about forEach and includes which makes perfect pair for tasks like this
 // Do not edit below this line
 module.exports = removeFromArray;
