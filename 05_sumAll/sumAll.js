5c472568fcc95404a65eb569d0c9a8ca3cd992fd
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-04-30 16:24:27 +0200

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (
+    min > 0 &&
+    max > 0 &&
+    typeof min === "number" &&
+    typeof max === "number"
+  ) {
+    if (min > max) {
+      for (max; max <= min; max++) {
+        console.log(min, max);
+        sum += max;
+      }
+    } else {
+      for (min; min <= max; min++) {
+        console.log(min, max);
+        sum += min;
+      }
+    }
+  } else return "ERROR";
+  console.log(sum);
+  return sum;
 };
+
+// Almost the same solution, I could use isInteger() method to filter type of variable from arguments and just use temp variable to switch values instead using two loops in if code blocks
 
 // Do not edit below this line
 module.exports = sumAll;
