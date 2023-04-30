b668067af30d4023a0e86ff360188300cae3ad6e
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-04-30 12:53:28 +0200

    Solve exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  if (number < 0) return "ERROR";
+  let combinedString = "";
+  while (number) {
+    combinedString += string;
 
+    number--;
+  }
+  return combinedString;
 };
 
 // Do not edit below this line
