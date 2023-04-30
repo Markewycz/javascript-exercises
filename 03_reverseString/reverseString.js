4c693217342a81fea370e4a987482b3812edf47e
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-04-30 13:08:12 +0200

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringIndex = string.length - 1;
+  let reversed = "";
+  while (stringIndex >= 0) {
+    reversed += string.charAt(stringIndex);
 
+    stringIndex--;
+  }
+  return reversed;
 };
 
 // Do not edit below this line
